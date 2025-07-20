b45afdaa79e785003c5d765793165443227da623
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-07-20 10:25:26 -0300

    feat: config app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
+      type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT!,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,

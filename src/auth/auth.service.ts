cae3bd8fd29914f4dc94ccc92a0e1d8c24cfee39
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-07-20 09:24:59 -0300

    feat: configure type and authentication props

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/entities/user.entity';
 
+type AuthenticatedUser = Omit<User, 'password'>;
 @Injectable()
 export class AuthService {
   constructor(
@@ -10,7 +12,10 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<AuthenticatedUser> {
     const user = await this.usersService.findByEmail(email);
     if (user && (await bcrypt.compare(password, user.password))) {
       const { password, ...result } = user;
@@ -19,7 +24,7 @@ export class AuthService {
     throw new UnauthorizedException('Invalid credentials');
   }
 
-  login(user: any) {
+  login(user: AuthenticatedUser) {
     const payload = { username: user.email, sub: user.id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
